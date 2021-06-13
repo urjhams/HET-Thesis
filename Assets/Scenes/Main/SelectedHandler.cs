6bd4d37ea135a3ef71bb09c58fb64597259751aa
Quân <quan@foolography.com>
2021-06-14 01:40:23 +0200

    add gaze aware for easy scene

---
+++ Assets/Scenes/Main/SelectedHandler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Tobii.Gaming;
+
+public class SelectedHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
